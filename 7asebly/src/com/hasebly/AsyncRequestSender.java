c304e083071cdbc2169c1f86cedd8c4ba7da4872
Omar El Sheikha <omssheikha@7asebly.com>
2014-03-15 15:41:30 +0200

    1. Finished all the requesthub.java requests by making 2. handled all the request reponces in the new class RequestHandler 3. added some things to AsyncRequestsender but it is yet to be complete

--- 7asebly/src/com/hasebly/AsyncRequestSender.java
+++ 7asebly/src/com/hasebly/AsyncRequestSender.java
@@ -1,5 +1,15 @@
 package com.hasebly;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+
 import android.os.AsyncTask;
 
 public class AsyncRequestSender extends AsyncTask<String, String, String>{
@@ -14,7 +24,15 @@ public class AsyncRequestSender extends AsyncTask<String, String, String>{
 
 	@Override
 	protected String doInBackground(String... parameters) {
-		// TODO Auto-generated method stub
+		HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+        DefaultHttpClient client = new DefaultHttpClient();
+        SchemeRegistry registry = new SchemeRegistry();
+        SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+        socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
+        registry.register(new Scheme("https", socketFactory, 443));
+        SingleClientConnManager mgr = new SingleClientConnManager(client.getParams(), registry);
+        DefaultHttpClient httpClient = new DefaultHttpClient(mgr, client.getParams());
+        HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
 		return null;
 	}
 	
--- 7asebly/src/com/hasebly/RequestHub.java
+++ 7asebly/src/com/hasebly/RequestHub.java
@@ -26,8 +26,8 @@ public class RequestHub {
 		return loginReply;
 	}
 
-	protected JSONObject pureMagneticTransaction(String cardHolder, String cardNumber, String expiryDate, 
-			double amount, String signature, String trackOne, String trackTwo) throws JSONException
+	protected JSONObject magneticTransaction(String cardHolder, String cardNumber, String expiryDate, 
+			double amount, String signature, String trackOne, String trackTwo, String code) throws JSONException
 	{
 		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
 		clearVaribaes.put("ch",cardHolder);
@@ -38,11 +38,115 @@ public class RequestHub {
 		clearVaribaes.put("t1",trackOne);
 		clearVaribaes.put("t2",trackTwo);
 		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		if(code != null)
+			clearVaribaes.put("code",code);
 		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
 				"https://account.7asebly.com/sphere/make_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
 		return new JSONObject(temp.responce);
 	}
 	
+	
+	protected JSONObject chipTransaction(String cardHolder, String cardNumber, String expiryDate, 
+			double amount, String signature,String trackTwo, String iccData, String code) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("ch",cardHolder);
+		clearVaribaes.put("cn",cardNumber);
+		clearVaribaes.put("ed",expiryDate);
+		clearVaribaes.put("a",toCurrencyString(amount));
+		clearVaribaes.put("image",signature);
+		clearVaribaes.put("t2",trackTwo);
+		clearVaribaes.put("icc",iccData);		
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		if(code != null)
+			clearVaribaes.put("code",code);
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/make_chip_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	protected JSONObject cashTransaction(String code, double amount, String cvv) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("a",toCurrencyString(amount));
+		clearVaribaes.put("image",cvv);
+		clearVaribaes.put("code",code);
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/make_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	protected JSONObject explicitMagneticTransaction(String cardHolder, String cardNumber, String expiryDate, 
+			double amount, String signature, String trackOne, String trackTwo, String recipient) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("ch",cardHolder);
+		clearVaribaes.put("cn",cardNumber);
+		clearVaribaes.put("ed",expiryDate);
+		clearVaribaes.put("a",toCurrencyString(amount));
+		clearVaribaes.put("image",signature);
+		clearVaribaes.put("t1",trackOne);
+		clearVaribaes.put("t2",trackTwo);
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		clearVaribaes.put("recipient", recipient);
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/make_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	
+	protected JSONObject explicitChipTransaction(String cardHolder, String cardNumber, String expiryDate, 
+			double amount, String signature,String trackTwo, String iccData, String recipient) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("ch",cardHolder);
+		clearVaribaes.put("cn",cardNumber);
+		clearVaribaes.put("ed",expiryDate);
+		clearVaribaes.put("a",toCurrencyString(amount));
+		clearVaribaes.put("image",signature);
+		clearVaribaes.put("t2",trackTwo);
+		clearVaribaes.put("icc",iccData);		
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		clearVaribaes.put("recipient", recipient);
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/make_chip_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	protected JSONObject explicitCashTransaction(String recipient, double amount, String cvv) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("a",toCurrencyString(amount));
+		clearVaribaes.put("image",cvv);
+		clearVaribaes.put("recipient",recipient);
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/make_transaction",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	protected JSONObject getInformationFromCode(String code) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("code",code);
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/get_transaction_details",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);
+	}
+	
+	protected JSONObject changeCodeStatus(String code, String update) throws JSONException
+	{
+		HashMap<String, String> clearVaribaes = new HashMap<String, String>();
+		clearVaribaes.put("code",code);
+		clearVaribaes.put("status",update);
+		clearVaribaes.put("xvalue", EncryptionVault.getSessionValue());
+		AsyncRequestSender temp = (AsyncRequestSender) new AsyncRequestSender().execute(
+				"https://account.7asebly.com/sphere/order_completed",Encryptor.encryptRequestVariables(clearVaribaes, Encryptor.AES));
+		return new JSONObject(temp.responce);	
+	}
+		
 	private String toCurrencyString(double amount)
 	{
 		NumberFormat formatter = NumberFormat.getCurrencyInstance();
