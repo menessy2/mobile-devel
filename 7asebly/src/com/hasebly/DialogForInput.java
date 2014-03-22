b1ec39d6c9a6f66138d244dac861162597c870da
Aya-S <aya0salama@gmail.com>
2014-03-22 03:36:26 +0200

    ammendments to dialogforinput and cardsnotpresentdata

--- 7asebly/src/com/hasebly/DialogForInput.java
+++ 7asebly/src/com/hasebly/DialogForInput.java
@@ -1,6 +1,6 @@
 package com.hasebly;
-
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -13,13 +13,52 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-@SuppressLint("NewApi")
 public class DialogForInput extends DialogFragment {
 	
 
-	private static EditText dataItem1, dataItem2; 
-	private static Editable data2_text;
-	 private static Editable data1_text;
+	protected class DialogView
+	{
+		EditText name;
+		EditText pan;
+		EditText cvv;
+		EditText expdate;
+		EditText email;
+		
+	}
+	
+	
+	public interface DialogListenerInterface {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        @SuppressLint("NewApi")
+		public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    
+    // Use this instance of the interface to deliver action events
+    DialogListenerInterface mListener;
+    static DialogView datacontainers ;
+
+    
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (DialogListenerInterface) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement DialogListenerInterface");
+        }
+    }
+
+	
+
+	static EditText dataItem1;
+	static EditText dataItem2; 
+	static Editable data2_text;
+	 static Editable data1_text;
 	 private int requestType;
 	 
 	 public DialogForInput() {
@@ -34,8 +73,7 @@ public class DialogForInput extends DialogFragment {
 	public void onDestroyView() {
 		// TODO Auto-generated method stub
 		System.out.println("inside onDestroy");
-		System.out.println(data2_text);
-		System.out.println(data1_text);
+	
 		//EditText pin = (EditText) findViewById(R.id.pin);
 		
 		super.onDestroyView();
@@ -57,29 +95,26 @@ public class DialogForInput extends DialogFragment {
 		 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		    // Get the layout inflater
 		    LayoutInflater inflater = getActivity().getLayoutInflater();
-		   
-		    View view = inflater.inflate(R.layout.dialog, null);
-		    dataItem1= (EditText) view.findViewById(R.id.name);
-		    dataItem2=(EditText) view.findViewById(R.id.pin);
-		    // Inflate and set the layout for the dialog
-		    // Pass null as the parent view because its going in the dialog layout
-		    
-		    //set the texts that appear in the dialog
-		    TextView text1 = (TextView) view.findViewById(R.id.dialog_request1_text);
-		    TextView text2 = (TextView) view.findViewById(R.id.dialog_request2_text);
-		    
-		    if (requestType == 1)
-		    {
-		    	text1.setText("Enter Name");
-		    	text2.setText("Enter Pin");
-		    	
-		    }else if(requestType== 2)
-		    {
-		    	text1.setText("Enter Email");
-		    	text2.setText("Enter CVV");
+		    View view =null;
+		    datacontainers = new DialogView();
+		    if (requestType == 2)
+		    {   // Inflate and set the layout for the dialog
+			    // Pass null as the parent view because its going in the dialog layout
+			    
+		    	 view = inflater.inflate(R.layout.dialog, null);
+			    	datacontainers.email= (EditText) view.findViewById(R.id.email);
+			    	datacontainers.cvv= (EditText) view.findViewById(R.id.cvv);
+				    
 		    }
-		    
-		    
+		    else if(requestType== 1)
+		    {
+		    	view = inflater.inflate(R.layout.dialogforpan, null);
+		    	datacontainers.name= (EditText) view.findViewById(R.id.name);
+		    	 datacontainers.pan=(EditText) view.findViewById(R.id.Pan);
+		    	 datacontainers.cvv=(EditText) view.findViewById(R.id.dialogforpan_cvv);
+		    	 datacontainers.expdate=(EditText) view.findViewById(R.id.expdate);
+		    }
+		   
 		    
 		    builder.setView(view)
 		    // Add action buttons
@@ -87,8 +122,8 @@ public class DialogForInput extends DialogFragment {
 		               @Override
 		               public void onClick(DialogInterface dialog, int id) {
 		                   // sign in the user ...
-		            	   DialogForInput.data2_text = DialogForInput.dataItem2.getText();
-		            	   DialogForInput.data1_text = DialogForInput.dataItem1.getText();
+		            	   mListener.onDialogPositiveClick(DialogForInput.this);
+		            	 
 		            	   
 		            	   
 		            	   
@@ -96,10 +131,15 @@ public class DialogForInput extends DialogFragment {
 		           })
 		           .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 		               public void onClick(DialogInterface dialog, int id) {
-		                   DialogForInput.this.getDialog().cancel();
+		         		  DialogForInput.this.getDialog().cancel();
+
+		                   
 		               }
 		           });      
 		    return builder.create();
 	}
+	
+	
+	
 
 }
